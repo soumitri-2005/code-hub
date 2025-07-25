14dd3074644845721f4e778afd5b554acbae2aaa
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-07-25 21:12:51 +0530

    making pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
 
 function App() {
-
   return (
     <>
-      <div className="App">
-        <h1>hello</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/EditorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditorPage = () => {
+  return (
+    <div>
+      editor page
+    </div>
+  )
+}
+
+export default EditorPage
