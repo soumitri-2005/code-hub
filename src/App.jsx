4e0a19814d593ac513e3b1c727a4221fe1686fbc
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-07-26 14:22:19 +0530

    functioning home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,32 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-center"
+          reverseOrder={true}
+          toastOptions={{
+            success: {
+              style: {
+                padding: "8px",
+                color: "var(--dark-one)",
+                fontSize: "13px",
+                fontFamily: "var(--font-style)",
+                fontWeight: "600",
+              },
+              iconTheme: {
+                primary: "#4aed88",
+                secondary: "var(--light-one)",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,50 @@
+import { useState } from "react";
 import logo from "/logo.png";
+import { v4 as uuid } from "uuid";
+import toast from "react-hot-toast";
+
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuid(); // here new id will be generated
+    setRoomId(id); // store the id in state
+    toast.success("New room created successfully!");
+  };
+
   return (
     <>
       <div className="home-page-wrapper">
         <div className="form-wrapper">
           <div className="logo">
             <img src={logo} alt="code-hub-logo" />
-            {/* <div className="line"></div> */}
             <div className="logo-name">
               <h1>Code Hub</h1>
-              {/* <p>Collaborative Code Editor</p> */}
             </div>
           </div>
           <h4 className="main-label">Paste invitation ROOM ID :</h4>
           <div className="input-group">
-            <input type="text" className="input-box" placeholder="ROOM ID" />
-            <input type="text" className="input-box" placeholder="USERNAME" />
+            <input
+              type="text"
+              className="input-box"
+              placeholder="ROOM ID"
+              onChange={(e) => setRoomId(e.target.value)}
+              value={roomId}
+            />
+            <input
+              type="text"
+              className="input-box"
+              placeholder="USERNAME"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
             <button className="btn join-btn">Join</button>
             <span className="create-info">
-              if you don't have an invite then create a new room &nbsp;
-              <a href="" className="create-new-btn">
-                new room
+              if you don't have an invite then create a
+              <a onClick={createNewRoom} href="" className="create-new-btn">
+                &nbsp;new room
               </a>
             </span>
           </div>
