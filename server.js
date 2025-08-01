9009b41390b1651ef5234d7e217ff5286d3f59a2
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-08-02 00:21:16 +0530

    deploying

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import ACTIONS from "./Action.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static("dist"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const userSocketMap = {};
 // exp
