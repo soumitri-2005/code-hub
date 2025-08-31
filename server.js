eed69e7f58b889b92468ac9ecad0ce69f654b15e
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-08-31 11:58:51 +0530

    redeploying

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "dist"))); 
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use((req, res, next) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
-})
+});
+
 
 const userSocketMap = {};
 
